f5039fe3f3fb601c8eb8dd3347c84b934be2807e
Mohamed Mahmoud <blackbird2013.44@gmail.com>
2024-10-12 00:30:45 +0300

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+// import autoprefixer from 'autoprefixer'
+// // const postcssCustomMedia = require('postcss-custom-media')
+
+// export const postcss = [
+//    //  postcssCustomMedia(),
+//    autoprefixer({
+//       grid: 'autoplace',
+//       Browserslist: {
+//          production: ['>0.2%', 'not dead', 'not op_mini all'],
+//          development: [
+//             'last 1 chrome version',
+//             'last 1 firefox version',
+//             'last 1 safari version',
+//             'last 1 ie version',
+//          ],
+//       },
+//    }),
+// ]
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+// /** @type {import('vite').UserConfig} */
+import { resolve } from 'path'
+// import { postcss } from './postcss.config.js'
+// import liveReload from 'vite-plugin-live-reload'
+
+export default {
+   // plugins: [liveReload('/**/*.index')],
+   server: {
+      port: 3000,
+      watch: {
+         usePolling: true,
+      },
+   },
+   root: 'src',
+   emptyOutDir: true,
+   build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      rollupOptions: {
+         input: {
+            main: resolve(__dirname, 'src/index.html'),
+            about: resolve(__dirname, 'src/about/index.html'),
+         },
+      },
+   },
+}
+// css: { postcss },
